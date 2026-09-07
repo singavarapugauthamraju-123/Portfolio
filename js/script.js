dd2d62b3402dfc2c3d1fb56e66577a1506e071f1
Gautham Raju <singavarapugauthamraju@gmail.com>
2026-09-07 12:31:19 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -159,7 +159,7 @@ performance support solutions in corporate environments.</li>
       label: "Adobe Animate",
       img: "assets/icons/skills/Animate_logo.webp",
       icon: "fa-brands fa-adobe",
-      type: "zip",
+      // type: "zip",
       desc: "Interactive HTML5 projects exported from Animate.",
       items: [
         { id: "an-1", label: "Zomato Banner", thumb: "assets/images/portfolio/Zomato.png", projectUrl: "animate/Zomato Banner/index.html"},
